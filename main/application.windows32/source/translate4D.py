8bd2add0db5f432962fdf315663a1b17834a8b5a
kaitlynng <kaitlyn.nky@gmail.com>
2018-09-29 12:11:20 +0800

    Added start screen and enabled restart game

--- main/application.windows32/source/translate4D.py
+++
@@ -1,5 +0,0 @@
-def translate4D(coor, transKey, direction):
-    trans_vec = [0,0,0,0]
-    trans_vec[transKey] = direction
-    shape_adj = list(a+b for a,b in zip(coor, trans_vec))
-    return shape_adj
