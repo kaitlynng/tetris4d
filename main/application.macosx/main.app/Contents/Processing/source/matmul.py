765cd83801abf15f37c6138b5a7bceba36c4d363
kaitlynng <kaitlyn.nky@gmail.com>
2018-09-29 12:10:26 +0800

    Added start screen and enabled restart game

---
+++ main/application.macosx/main.app/Contents/Processing/source/matmul.py
@@ -0,0 +1,11 @@
+def matmul(mat1,mat2):
+    #matrix multiplication code, because processing doesn't have numpy support
+    
+	rows1, cols1, rows2, cols2 = len(mat1), len(mat1[0]), len(mat2), len(mat2[0])
+
+	if cols1 != rows2 :
+		print( "matrixes of wrong size, mat1 is {}x{}, mat2 is {}x{}".format(rows1, cols1,rows2,cols2))
+		return
+	mat_out = [[sum(a*b for a,b in zip(mat1_row, mat2_col)) for mat2_col in zip(*mat2)] for mat1_row in mat1]
+
+	return mat_out
